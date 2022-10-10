0932e0f8e55649e68493cfa472b6f7cc8f11a69f
mamudou1 <mamudoubojang17@gmail.com>
2022-10-10 09:37:08 +0000

    day four

---
+++ booleaen.py
@@ -0,0 +1,9 @@
+#print(10>12)
+#print(1==1)
+def myf() :
+ return False
+print(myf())
+
+#use of in built function
+y=200
+print(isinstance(y, int))
---
+++ lists.py
@@ -0,0 +1,9 @@
+from traceback import print_tb
+
+
+mylsir=["mango", "bna", "orange","apple"]
+#to print the lenght of the list
+print(len(mylsir))
+print(mylsir)
+print(type(mylsir))
+print(mylsir[3])
